f87424c90e055e824d5e8d53fa85c60be8f52d46
tallaev <98256797+tallaev@users.noreply.github.com>
2024-05-08 15:40:16 +0700

    Add files via upload

---
+++ 8_2/lab 8.2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace lab_8._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAsk = new System.Windows.Forms.Button();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(15, 50);
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(270, 20);
+            this.txtQuestion.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите свой вопрос";
+            // 
+            // btnAsk
+            // 
+            this.btnAsk.FlatAppearance.BorderSize = 0;
+            this.btnAsk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAsk.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAsk.Location = new System.Drawing.Point(101, 86);
+            this.btnAsk.Name = "btnAsk";
+            this.btnAsk.Size = new System.Drawing.Size(75, 23);
+            this.btnAsk.TabIndex = 2;
+            this.btnAsk.Text = "Ответ";
+            this.btnAsk.UseVisualStyleBackColor = true;
+            this.btnAsk.Click += new System.EventHandler(this.btnAsk_Click);
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAnswer.Location = new System.Drawing.Point(62, 119);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(0, 21);
+            this.labelAnswer.TabIndex = 3;
+            this.labelAnswer.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 159);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.btnAsk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQuestion);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAsk;
+        private System.Windows.Forms.Label labelAnswer;
+    }
+}
+
---
+++ 8_2/lab 8.2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_8._2
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, double> answers = new Dictionary<string, double>()
+        {
+            { "Да, определенно", 0.2 }, //x<0.2
+            { "Нет, ни в коем случае", 0.1 }, //0.2<=x<0.3
+            { "Может быть", 0.3 }, //0.3<=x<0.6
+            { "Попробуйте снова позже", 0.2 }, //0.6<=x<0.8
+            { "Сомнительно", 0.2 } //x>=0.8
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAsk_Click(object sender, EventArgs e)
+        {
+            string question = txtQuestion.Text;
+
+            if (!string.IsNullOrEmpty(question))
+            {
+                string answer = GetRandomAnswer();
+                MessageBox.Show(answer, "Ответ шара предсказаний");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите вопрос.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string GetRandomAnswer()
+        {
+            double totalProbability = answers.Values.Sum();
+            
+            double randomValue = new Random().NextDouble() * totalProbability;
+
+            double cumulative = 0;
+
+            foreach (var pair in answers)
+            {
+                cumulative += pair.Value;
+                if (randomValue < cumulative)
+                    return pair.Key;
+            }
+
+            return answers.Keys.Last();
+        }
+    }
+}
